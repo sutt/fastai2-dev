bae4c8b30cafbf110e3f70f5c459666f0264e25a
Will Sutton <wsutton17@gmail.com>
2020-04-29 16:05:23 -0400

    up

--- chess-classification-hw/modules/plotutils.py
+++ chess-classification-hw/modules/plotutils.py
@@ -34,21 +34,35 @@ def load_many_imgs(d, N=10):
     print(f"loaded {len(imgs)} images")
     return imgs
 
-def scree(learn):
+class ScreePlot:
     
-    def foo(cv, ct):
-        plt.plot(pd.DataFrame(learn.recorder.values).iloc[:,cv], label='valid')
-        plt.plot((pd.DataFrame(learn.cbs[3].values).iloc[:,ct]),label='test' )
-    
-    foo(cv=2,ct=1)
-    plt.ylim(0,1)
-    plt.legend()
-    plt.title('acc')
-    plt.show()
-    
-    foo(cv=1,ct=0)
-    plt.legend()
-    plt.title('loss')
-    plt.ylim(0,3)
-    plt.show()
+    def __init__(self):
+        self.values = None
+
+    def save(self, learn):
+        self.values = pd.DataFrame(learn.recorder.values.copy())
+
+    def scree(self, learn):
+        
+        valid_df = pd.DataFrame(learn.recorder.values)
+        test_df = pd.DataFrame(learn.cbs[3].values)
+
+        if self.values is not None:
+            valid_df = pd.concat((self.values, valid_df))
+
+        def foo(cv, ct):
+            plt.plot(valid_Df.iloc[:,cv], label='valid')
+            plt.plot((test_df.iloc[:,ct]),label='test' )
+        
+        foo(cv=2,ct=1)
+        plt.ylim(0,1)
+        plt.legend()
+        plt.title('acc')
+        plt.show()
+        
+        foo(cv=1,ct=0)
+        plt.legend()
+        plt.title('loss')
+        plt.ylim(0,3)
+        plt.show()
     
